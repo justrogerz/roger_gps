ccc1b1529a6367d03849777e4e4ba0dd2b57adb7
!Roger <85873462+justrogerz@users.noreply.github.com>
2022-06-04 19:37:17 +0200

    added files

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+author '!Roger#4702'
+description 'Comando /gps <civico>'
+version '1.0.0'
+
+shared_script '@es_extended/imports.lua'
+
+client_script 'client/gps.lua'
