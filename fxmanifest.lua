773f135881255de47ca81f481e37a6d963874f15
!Roger <85873462+justrogerz@users.noreply.github.com>
2022-06-04 19:53:38 +0200

    fix manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,4 @@ game 'gta5'
 author '!Roger#4702'
 description 'Comando /gps <civico>'
 version '1.0.0'
-
-shared_script '@es_extended/imports.lua'
-
-client_script 'client/gps.lua'
+client_script 'gps.lua'
